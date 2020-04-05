1930578314f44604578846101a4c344d5c80b7da
Archit Gupta <guptaarchit023@gmail.com>
2020-04-05 12:51:08 +0530

    changed api

--- india.js
+++ india.js
@@ -7,17 +7,12 @@ var request = require("request");
 var p;
 var options = {
   method: 'GET',
-  url: 'https://corona-virus-world-and-india-data.p.rapidapi.com/api_india',
-  headers: {
-    'x-rapidapi-host': 'corona-virus-world-and-india-data.p.rapidapi.com',
-    'x-rapidapi-key': '7e0001ce62mshea562a1794af41ap1623d1jsn02489908e1e2'
-  }
+  url: 'https://api.covid19india.org/data.json',
 };
 
 request(options, function (error, response, body) {
 	if (error) throw new Error(error);
       p = JSON.parse(body);
-	console.log(p);
 });
 app.get("/",function(req,res)
 {

aec6f6bb2bd78b1977f77d34589bce25d6dd714c
priteshsingh-vats <42402753+priteshsingh-vats@users.noreply.github.com>
2020-04-05 21:56:26 +0530

    Update india.js
    updated for the code for adding graphs

--- india.js
+++ india.js
@@ -5,20 +5,40 @@ var bodyparser = require("body-parser");
 app.use(bodyparser.urlencoded({extended:true}));
 var request = require("request");
 var p;
+var state=[];
+var scase =[];
+var  labels=[];
+var  data =[];
 var options = {
   method: 'GET',
   url: 'https://api.covid19india.org/data.json',
 };
-
 request(options, function (error, response, body) {
 	if (error) throw new Error(error);
       p = JSON.parse(body);
+      for(var i=31;i<p.cases_time_series.length;i++) 
+   {
+  labels[i-31] =p.cases_time_series[i].date;
+ data[i-31] =p.cases_time_series[i].totalconfirmed;
+ }
+ for(var i=1;i<p.statewise.length;i++)
+ {
+   state[i-1]=p.statewise[i].state;
+   scase[i-1] =p.statewise[i].confirmed;
+ }
 });
+
 app.get("/",function(req,res)
 {
-   res.render("covidworld.ejs",{p:p});
+   res.render("covidworld.ejs",{p:p,
+  data:data,
+  labels:labels,
+  state:state,
+  scase:scase,
+  })
 });
 app.listen((process.env.PORT||3000),function()
 {
     console.log("The server has started");
 });
+

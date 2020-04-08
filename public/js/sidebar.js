bca2a99d8cf708aa9f9d2fe088314448fda27495
Archit Gupta <guptaarchit023@gmail.com>
2020-04-08 21:02:40 +0530

    marquee and navbar

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -7,3 +7,9 @@ function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
 }
+
+var searchClick = function () {
+    var text = document.getElementById("text-form").value;
+    // alert(text);
+    window.location.href = "www.<some-url>" + text;
+}
